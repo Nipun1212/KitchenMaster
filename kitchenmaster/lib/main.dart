dd0b34009e579f9f8269160860257fc72e4c3815
EnriqueJuspi <enriquejuspi@gmail.com>
2023-03-07 20:50:58 +0800

    Initial Flutter Firebase Integration
    Requirement:
    - Download and Install Firebase CLI
    - Download and Install Latest NodeJS

--- kitchenmaster/lib/main.dart
+++ kitchenmaster/lib/main.dart
@@ -1,11 +1,15 @@
 // ignore_for_file: prefer_const_constructors
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'register.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
