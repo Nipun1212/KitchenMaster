a81b1c0bc8e1b40193ed4af55001a3aea5d98563
Nusra7479 <nusrath.hajara@gmail.com>
2023-03-30 22:02:57 +0800

    Added IOS settings

--- kitchenmaster/lib/alerts.dart
+++ kitchenmaster/lib/alerts.dart
@@ -7,6 +7,8 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/timezone.dart';
 import 'package:timezone/data/latest.dart' as tz;
+import 'dart:io' show Platform;
+import 'package:intl/intl.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 // import 'profile.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
@@ -23,6 +25,7 @@ class DynamicWidget extends StatefulWidget {
   String frequency = '';
   TextEditingController nameController = TextEditingController();
   String id = '';
+  int intId = 0;
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   DynamicWidget(TextEditingController n, String value) {
@@ -30,15 +33,31 @@ class DynamicWidget extends StatefulWidget {
     name = n.text;
     frequency = value;
     id = UniqueKey().toString();
+    intId = 0;
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   }
 
+  // // Generate a unique integer value for a notification
+  // int generateUniqueId(String uniqueId) {
+  //   var now = DateTime.now();
+  //   var formatter = DateFormat('yyyyMMddHHmmss');
+  //   var formattedDate = formatter.format(now);
+  //   return int.parse(formattedDate + uniqueId.hashCode.toString());
+  // }
+
   void deleteNotifications() async {
+    if (Platform.isAndroid) {
     await flutterLocalNotificationsPlugin.cancelAll();
     await flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<
-        AndroidFlutterLocalNotificationsPlugin>()
+    AndroidFlutterLocalNotificationsPlugin>()
         ?.deleteNotificationChannel(id);
+    }
+    else if (Platform.isIOS){
+      // UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [id]);
+      await flutterLocalNotificationsPlugin.cancel(intId); // TODO
+      }
+
   }
 
   void deleteNotificationFirebase() async {
@@ -95,6 +114,7 @@ class DynamicWidget extends StatefulWidget {
       presentAlert: true,
       presentSound: true,
       badgeNumber: 1,
+      threadIdentifier: id,
     );
 
     var platformDetails = NotificationDetails(
@@ -104,7 +124,7 @@ class DynamicWidget extends StatefulWidget {
 
     // Schedule the notification
     await flutterLocalNotificationsPlugin.zonedSchedule(
-      0, // notification id
+      intId, // notification id
       'Grocery List Reminder', // notification title
       'Restock ' + name + '!', // notification body
       _nextInstanceOfTimeOfDay(freqInDays), // scheduled date and time
@@ -347,12 +367,17 @@ class _AlertsPageState extends State<AlertsPage> {
         .resolvePlatformSpecificImplementation<
         AndroidFlutterLocalNotificationsPlugin>()
         ?.getNotificationChannels();
-    //
+
+    List<PendingNotificationRequest> Notifications =
+    await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+
     setState(() {
       snapshot.docs.forEach((document) {
         // Extract the data from the document
         final data = document.data();
         final id = document.id;
+        int? intId = data['intID'];
+        if (intId == null) intId = 0; // TODO check
         final name = data['name'];
         final frequency = data['frequency'];
 
@@ -371,17 +396,41 @@ class _AlertsPageState extends State<AlertsPage> {
         controllers.add(n);
         DynamicWidget oldAlert = DynamicWidget(n, frequency);
         oldAlert.id = id;
+        oldAlert.intId = intId;
         listCards.add(oldAlert);
 
         //if instance not found in device, add
-        if (channels != null) {
-          bool channelExists = channels.any((channel) => channel.id == id);
-          debugPrint(id + " " + channelExists.toString());
-          if (channelExists) return;
+        if (Platform.isAndroid) {
+          if (channels != null) {
+            bool channelExists = channels.any((channel) => channel.id == id);
+            debugPrint(id + " " + channelExists.toString());
+            if (channelExists) return;
+          }
+        }
+        if (Platform.isIOS){
+          if (Notifications != null) {
+            bool notificationExists = Notifications.any((
+                notification) => notification.id == intId);
+            debugPrint(id + " " + notificationExists.toString());
+            if (notificationExists) return;
+          }
         }
         oldAlert.scheduleNotification(id, name, frequency);
       });
     });
+  }
+
+  // Generate a unique integer value for a notification
+  int generateUniqueId(String uniqueId) {
+    var now = DateTime.now();
+    var formatter = DateFormat('yyMMddHHmmss');
+    var formattedDate = formatter.format(now);
+    return int.parse(formattedDate);
+    // return int.parse(formattedDate + uniqueId.hashCode.toString());
+  }
+  int generateTimestampId() {
+    DateTime now = DateTime.now();
+    return now.millisecondsSinceEpoch;
   }
 
   Future<void> addDynamic(TextEditingController n, String value) async {
@@ -392,10 +441,15 @@ class _AlertsPageState extends State<AlertsPage> {
       'name': '',
       'frequency': 'None'
     });
+    // final newAlertId = generateUniqueId(newAlertRef.id);
+    final newAlertId = newAlertRef.id.hashCode;
+    await newAlertRef.update({'intId': newAlertId});
+
     setState(() {
       var dynamicWidget = DynamicWidget(n, value);
       controllers.add(n);
       dynamicWidget.id = newAlertRef.id;
+      dynamicWidget.intId = newAlertId;
       listCards.add(dynamicWidget);
       log("added dynamic");
     });
@@ -403,16 +457,16 @@ class _AlertsPageState extends State<AlertsPage> {
 
   }
 
-  void resetDynamic() {
-    setState(() {
-      // if (listCards.isEmpty){
-      //   //show error message
-      // }
-      controllers.removeRange(0, controllers.length);
-      listCards.removeRange(0, listCards.length);
-    });
-  }
-
+  // void resetDynamic() {
+  //   setState(() {
+  //     // if (listCards.isEmpty){
+  //     //   //show error message
+  //     // }
+  //     controllers.removeRange(0, controllers.length);
+  //     listCards.removeRange(0, listCards.length);
+  //   });
+  // }
+  //
   void removeNoName() {
     setState(() {
       for (var i = 0; i < listCards.length; i++) {
