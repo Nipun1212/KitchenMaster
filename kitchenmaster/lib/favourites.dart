820df7a52296d9ad53c443f6534dd02ce0b80603
Joey Quah <joeyquah09@gmail.com>
2023-03-30 16:28:39 +0800

    indiv recipes

--- kitchenmaster/lib/favourites.dart
+++ kitchenmaster/lib/favourites.dart
@@ -5,6 +5,7 @@ import 'package:favorite_button/favorite_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fridgemaster/recipes.dart';
+import 'indivRecipes.dart';
 
 class FavouritesPage extends StatefulWidget {
   FavouritesPage({Key? key}) : super(key: key);
@@ -14,7 +15,37 @@ class FavouritesPage extends StatefulWidget {
 }
 
 class _FavouritesPageState extends State<FavouritesPage> {
-  List<String> favourites = RecipePage.getFavourites();
+  // List<String> favourites = RecipePage.getFavourites();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final CollectionReference recipes =
+      FirebaseFirestore.instance.collection('Recipes');
+
+  List savedRecipes = [];
+
+  Future<List> fetchSavedRecipes() async {
+    print("fetching saved");
+    QuerySnapshot querySnapshot = await recipes.get();
+    List<dynamic> recipeDB;
+    savedRecipes = [];
+    querySnapshot.docs.forEach((document) {
+      // check if saved function is saved
+      if (document["saved"]) {
+        //prints the name of recipes that matches the food items in fridge
+        print(document.get("Name"));
+        savedRecipes.add(document);
+      } else {}
+    });
+    // if (savedRecipes.length <= 0){
+    // }
+    // else {
+    return await savedRecipes;
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,56 +64,122 @@ class _FavouritesPageState extends State<FavouritesPage> {
               height: 1)),
       SizedBox(height: 30),
       Flexible(
-          fit: FlexFit.tight,
-          child: StreamBuilder(
-            stream:
-                FirebaseFirestore.instance.collection('Recipes').snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              return ListView(
-                children: snapshot.data!.docs
-                    .where((x) => favourites.contains(x['Name']))
-                    .map((document) {
-                  return Card(
-                      elevation: 0,
-                      color: Color.fromARGB(0, 255, 255, 255),
-                      child: Center(
-                          child: SizedBox(
-                              width: 350,
-                              height: 60,
-                              child: Column(children: <Widget>[
-                                Row(children: <Widget>[
-                                  TextButton(
-                                    child: Text(document['Name']),
-                                    onPressed: () {
-                                      // navigate to indiv recipe page
-                                    },
-                                  ),
-                                  FavoriteButton(
-                                    isFavorite: true,
-                                    valueChanged: (_isFavorite) {
-                                      if (_isFavorite) {
-                                        RecipePage.addFavourites(
-                                            document['Name']);
-                                        print(favourites);
-                                      } else if (!_isFavorite) {
-                                        RecipePage.removeFavourites(
-                                            document['Name']);
-                                        print(favourites);
-                                      }
-                                    },
-                                  )
-                                ])
-                              ]))));
-                }).toList(),
-              );
-            },
-          ))
+        fit: FlexFit.tight,
+        child: FutureBuilder<List>(
+          future: fetchSavedRecipes(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Container(
+                  child: Text("No Recipes Saved to Favourites Yet!"));
+            } else {
+              return snapshot.connectionState == ConnectionState.waiting
+                  ? const CircularProgressIndicator()
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: List.generate(
+                        snapshot.data!.length,
+                        (index) {
+                          return Card(
+                              elevation: 0,
+                              color: Color.fromARGB(0, 255, 255, 255),
+                              child: Center(
+                                  child: SizedBox(
+                                      width: 350,
+                                      height: 60,
+                                      child: Row(children: <Widget>[
+                                        TextButton(
+                                          child: Text(snapshot.data?[index]
+                                                  .get("Name") ??
+                                              "null"),
+                                          onPressed: () {
+                                            String recipeName = snapshot
+                                                .data![index]
+                                                .get("Name");
+                                            List<dynamic> ingredients = snapshot
+                                                .data![index]
+                                                .get("Ingredients");
+                                            String procedure = snapshot
+                                                .data![index]
+                                                .get("Procedures");
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        indivRecipePage(
+                                                            recipeName:
+                                                                recipeName,
+                                                            ingredients:
+                                                                ingredients,
+                                                            procedure:
+                                                                procedure)));
+                                          },
+                                        ),
+                                        // FavoriteButton(
+                                        //   valueChanged: (_isFavorite) {
+                                        //     if (_isFavorite) {
+                                        //       snapshot.data![index].update({"saved": true});
+                                        //     } else if (!_isFavorite) {
+                                        //       snapshot.data![index].update({"saved": false});
+                                        //     }
+                                        //   },
+                                        // )
+                                      ]))));
+                        },
+                      ),
+                    );
+            }
+          },
+        ),
+      )
     ]))));
   }
 }
+
+
+
+
+// Flexible(
+//           fit: FlexFit.tight,
+//           child: StreamBuilder(
+//             stream:
+//                 FirebaseFirestore.instance.collection('Recipes').snapshots(),
+//             builder:
+//                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+//               if (!snapshot.hasData) {
+//                 return Center(
+//                   child: CircularProgressIndicator(),
+//                 );
+//               }
+//               return ListView(
+//                 children: snapshot.data!.docs
+//                     .where((x) => x['saved'] == true)
+//                     .map((document) {
+//                   return Card(
+//                       elevation: 0,
+//                       color: Color.fromARGB(0, 255, 255, 255),
+//                       child: Center(
+//                           child: SizedBox(
+//                               width: 350,
+//                               height: 60,
+//                               child: Column(children: <Widget>[
+//                                 Row(children: <Widget>[
+//                                   TextButton(
+//                                     child: Text(document['Name']),
+//                                     onPressed: () {
+//                                       // navigate to indiv recipe page
+//                                     },
+//                                   ),
+//                                   FavoriteButton(
+//                                     isFavorite: true,
+//                                     valueChanged: (_isFavorite) {
+//                                       if (_isFavorite) {
+//                                       } else if (!_isFavorite) {}
+//                                     },
+//                                   )
+//                                 ])
+//                               ]))));
+//                 }).toList(),
+//               );
+//             },
+//           ))
--- kitchenmaster/lib/indivRecipes.dart
+++ kitchenmaster/lib/indivRecipes.dart
@@ -6,7 +6,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class indivRecipePage extends StatefulWidget {
-  indivRecipePage({Key? key}) : super(key: key);
+  indivRecipePage(
+      {super.key,
+      required this.recipeName,
+      required this.ingredients,
+      required this.procedure});
+
+  String recipeName;
+  List<dynamic> ingredients;
+  String procedure;
 
   @override
   State<indivRecipePage> createState() => _indivRecipePageState();
@@ -18,17 +26,91 @@ class _indivRecipePageState extends State<indivRecipePage> {
     return MaterialApp(
         home: Scaffold(
             body: Container(
-                child: Column(children: const <Widget>[
-                    SizedBox(height: 50),
-                    Text('Individual Recipe',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Color.fromRGBO(0, 0, 0, 1),
-                            fontFamily: 'Inria Serif',
-                            fontSize: 35,
-                            fontWeight: FontWeight.normal,
-                            height: 1)),
-                    SizedBox(height: 30),
-    ]))));
+                child:Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+              setState(() {});
+            }
+          )),
+          Text(widget.recipeName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Inria Serif',
+                  fontSize: 35,
+                  fontWeight: FontWeight.normal,
+                  height: 1)),
+          SizedBox(height: 30),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding:EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Text("Ingredients: ",
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Inria Serif',
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  height: 1)
+          ))),
+          Flexible(
+            fit: FlexFit.loose,
+            child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: widget.ingredients.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding:EdgeInsets.fromLTRB(20, 0, 0, 0),
+                child:Text("• ${widget.ingredients[index]}",
+                  style: TextStyle(
+                      color: Color.fromRGBO(0, 0, 0, 1),
+                      fontFamily: 'Inria Serif',
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      height: 1)));
+            }
+          )),
+          // Text("Ingredients: ${widget.ingredients.toString()}",
+          //     style: TextStyle(
+          //         color: Color.fromRGBO(0, 0, 0, 1),
+          //         fontFamily: 'Inria Serif',
+          //         fontSize: 20,
+          //         fontWeight: FontWeight.normal,
+          //         height: 1)),
+          SizedBox(height: 30),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding:EdgeInsets.fromLTRB(20, 0, 0, 10),
+              child: Text("Procedure: ",
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontFamily: 'Inria Serif',
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                  height: 1)
+          ))),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding:EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child:Text("${widget.procedure}",
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Inria Serif',
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    height: 1)
+          ))),
+          SizedBox(height: 30),
+        ]))));
   }
 }
